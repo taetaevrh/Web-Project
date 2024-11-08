5aff16a953662383188c2253eac354fb1436107e
taetae2004 <varich.malee@gmail.com>
2024-11-08 15:23:29 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const db = require("mysql2");
+const path = require("path");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const router = express.Router();
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.use(router);
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors({
+    origin: ["localhost:5500"],
+    credentials: true,
+}));
+
+router.get("/", (req, res) => {
+    console.log(`Request at ${req.url}`);
+    res.sendFile(path.join(__dirname, "html", "home.html"));
+});
+
+router.get("/about-us", (req, res) => {
+    console.log(`Request at ${req.url}`);
+    res.sendFile(path.join(__dirname, "html", "about-us.html"));
+});
+
+router.get("/contact", (req, res) => {
+    console.log(`Request at ${req.url}`);
+    res.sendFile(path.join(__dirname, "html", "contact.html"))
+});
+
+app.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Server is running on port: ${PORT}`);
+});
