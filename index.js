7a66b98df78ea8ddc83260e31de3e11b0e795103
taetae2004 <varich.malee@gmail.com>
2024-11-11 10:31:22 +0700

    update

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-const express = require("express");
-const db = require("mysql2");
-const path = require("path");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
+const express = require("express")
+const db = require("mysql2")
+const path = require("path")
+const dotenv = require("dotenv")
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
 
 const app = express();
-const router = express.Router();
+const router = express.Router()
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 app.use(router);
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(express.json())
 app.use(cors({
     origin: ["localhost:5500"],
     credentials: true,
 }));
 
 router.get("/", (req, res) => {
-    console.log(`Request at ${req.url}`);
-    res.sendFile(path.join(__dirname, "html", "home.html"));
+    console.log(`Request at ${req.url}`)
+    res.sendFile(path.join(__dirname, "html", "home.html"))
 });
 
 router.get("/about-us", (req, res) => {
-    console.log(`Request at ${req.url}`);
-    res.sendFile(path.join(__dirname, "html", "about-us.html"));
+    console.log(`Request at ${req.url}`)
+    res.sendFile(path.join(__dirname, "html", "about-us.html"))
 });
 
 router.get("/contact", (req, res) => {
-    console.log(`Request at ${req.url}`);
+    console.log(`Request at ${req.url}`)
     res.sendFile(path.join(__dirname, "html", "contact.html"))
 });
 
+router.get("/login", (req, res) => {
+    console.log(`Request at ${req.url}`)
+    res.sendFile(path.join(__dirname, "html", "login.html"))
+})
+
 app.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`Server is running on port: ${PORT}`);
+    if (err) throw err
+    console.log(`Server is running on port: ${PORT}`)
 });
